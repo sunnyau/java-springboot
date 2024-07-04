72cf5a38945c321471c155dcdaf7015768d5d919
sunnyau <sunny.au@outlook.com>
2024-07-04 13:56:01 +0100

    /person/all returns data in data.sql successfully

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-insert into Person (id, name, title) values (1, 'My name', 'My title');
+insert into Person (id, name, title) values (1, 'Andy', 'Tennis player');
+insert into Person (id, name, title) values (2, 'Bart', 'Footballer');
+insert into Person (id, name, title) values (3, 'Carl', 'Basketball player');
+insert into Person (id, name, title) values (4, 'Dennis', 'Swimmer');
