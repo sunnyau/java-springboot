4fef1ded0ad9e190bc2697999a44054b55a5bb73
sunnyau <sunny.au@outlook.com>
2024-07-04 14:21:18 +0100

    add test using MockMvc

---
+++ src/test/java/com/example/springboot/controller/HealthControllerMvcTest.java
@@ -0,0 +1,39 @@
+package com.example.springboot.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * A test using MockMvc
+ * 
+ * @throws Exception
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class HealthControllerMvcTest {
+
+    @InjectMocks
+    private HealthController healthController;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(healthController).build();
+    }
+
+    @Test
+    public void shouldReturnHealth() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("healthy"));
+    }
+}
--- src/test/java/com/example/springboot/controller/HealthControllerTest.java
+++ src/test/java/com/example/springboot/controller/HealthControllerTest.java
@@ -10,54 +10,19 @@ import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-// import org.junit.Before;
-// import org.junit.Test;
-// import org.junit.runner.RunWith;
-// import org.mockito.InjectMocks;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.context.junit4.SpringRunner;
-// import org.springframework.test.web.servlet.MockMvc;
-// import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-// import static com.atlassian.oai.validator.mockmvc.OpenApiValidationMatchers.openApi;
-// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+/**
+ * A test using TestRestTemplate
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HealthControllerTest {
 
-	@Autowired
-	private TestRestTemplate template;
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
     public void getHealth() throws Exception {
         ResponseEntity<String> response = template.getForEntity("/health", String.class);
-        // assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot!");
         assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(response.getBody()).isEqualTo("healthy");
     }
 }
-
-// @SpringBootTest
-// @AutoConfigureMockMvc
-// public class HealthControllerTest {
-
-//     private static String OPEN_API_DEFINITION = "./openapi.yml";
-
-//     @InjectMocks
-//     private HealthController healthController;
-//     private MockMvc mockMvc;
-
-//     @Before
-//     public void setUp() {
-//         mockMvc = MockMvcBuilders.standaloneSetup(healthController).build();
-//     }
-
-//     @Test
-//     public void shouldReturnHealth() throws Exception {
-//         mockMvc.perform(get("/health"))
-//                 .andExpect(status().isOk())
-//                 .andExpect(content().string("healthy"));
-//                 //.andExpect(openApi().isValid(OPEN_API_DEFINITION));
-//     }
-// }
