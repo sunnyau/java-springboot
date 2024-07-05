60b691d976fc9d216cb484c1d15885c879598aff
sunnyau <sunny.au@outlook.com>
2024-07-05 01:13:29 +0100

    tidy up tests. upgrade spring-boot-starter-parent

--- src/test/java/com/example/springboot/controller/PersonControllerMvcTest.java
+++ src/test/java/com/example/springboot/controller/PersonControllerMvcTest.java
@@ -1,16 +1,11 @@
 package com.example.springboot.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-// import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -29,13 +24,17 @@ public class PersonControllerMvcTest {
 
     // @BeforeEach
     // public void setUp() {
-    //     mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    // mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     // }
 
     @Test
     public void testGetMethod() throws Exception {
         String expectedContent = """
-                                 [{"id":1,"name":"Edward","title":"Racing Driver"},{"id":1001,"name":"Andy","title":"Tennis player"},{"id":1002,"name":"Bart","title":"Footballer"},{"id":1003,"name":"Carl","title":"Basketball player"},{"id":1004,"name":"Dennis","title":"Swimmer"}]""";
+                [{"id":1,"name":"Edward","title":"Racing Driver"},\
+                {"id":1001,"name":"Andy","title":"Tennis player"},\
+                {"id":1002,"name":"Bart","title":"Footballer"},\
+                {"id":1003,"name":"Carl","title":"Basketball player"},\
+                {"id":1004,"name":"Dennis","title":"Swimmer"}]""";
         mockMvc.perform(get("/person/all"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(expectedContent));
@@ -43,9 +42,11 @@ public class PersonControllerMvcTest {
 
     @Test
     public void testPostMethod() throws Exception {
+        String content = """
+                {"name":"Edward","title":"Racing Driver"}""";
         String expectedContent = """
-            {"id":1,"name":"Edward","title":"Racing Driver"}""";
-        mockMvc.perform(post("/person").contentType(MediaType.APPLICATION_JSON).content("{\"name\":\"Edward\",\"title\":\"Racing Driver\"}"))
+                {"id":1,"name":"Edward","title":"Racing Driver"}""";
+        mockMvc.perform(post("/person").contentType(MediaType.APPLICATION_JSON).content(content))
                 .andExpect(status().isOk())
                 .andExpect(content().string(expectedContent));
     }
